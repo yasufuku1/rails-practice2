17f1518c4306755f09e55f9a74e3b85c7483dd09
EC2 Default User <ec2-user@ip-172-31-4-148.ap-northeast-1.compute.internal>
2022-11-02 09:21:30 +0000

    [Finish]3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,6 +20,11 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
   private
   def list_params
     params.require(:list).permit(:title, :body)
